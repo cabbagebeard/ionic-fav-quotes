73e1860574d3706a0f4f8e78b19b3f26b807b482
Kevin Windisch <kwindisch1992@gmail.com>
2017-09-18 12:45:10 -0500

    Set up linking and begin quote template

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,25 +1,18 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NavParams} from "ionic-angular";
+import {Quote} from "../../data/quote.interface";
 
-/**
- * Generated class for the QuotesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
 export class QuotesPage {
+  quoteGroup: {category: string, quotes: Quote[], icon: string}
+  constructor(private navParams: NavParams) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+    this.quoteGroup = this.navParams.data;
   }
-
 }
