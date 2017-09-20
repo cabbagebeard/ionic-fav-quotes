b2036ba1790cd63c0162685cf599ae4f822486e0
Kevin Windisch <kwindisch1992@gmail.com>
2017-09-20 15:05:27 -0500

    Create alert popup for favoriting

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NavParams} from "ionic-angular";
+import {AlertController, NavParams} from "ionic-angular";
 import {Quote} from "../../data/quote.interface";
 
 @Component({
@@ -8,11 +8,22 @@ import {Quote} from "../../data/quote.interface";
 })
 export class QuotesPage implements OnInit {
   quoteGroup: {category: string, quotes: Quote[], icon: string}
-  constructor(private navParams: NavParams) {
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController) {
 
   }
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to favorite this quote?'
+    });
+    alert.present();
   }
 }
